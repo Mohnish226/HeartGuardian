d7f041e76223afa6247d5bac6c510c5f4cbfae0a
Mohnish Devadiga <mohnish226@yahoo.co.in>
2020-12-13 04:55:43 +1100

    Basic Operations Completed

--- API/PRIVATE/KEYS.py
+++ API/PRIVATE/KEYS.py
@@ -1,2 +1,3 @@
 PASSWORD = ""
+URL1 = "mongodb://mohnish:"+PASSWORD+"@cluster0-shard-00-00.omzxj.mongodb.net:27017,cluster0-shard-00-01.omzxj.mongodb.net:27017,cluster0-shard-00-02.omzxj.mongodb.net:27017/test?ssl=true&replicaSet=atlas-eo6ves-shard-0&authSource=admin&retryWrites=true&w=majority"
 URL = "mongodb+srv://mohnish:"+PASSWORD+"@cluster0.omzxj.mongodb.net/test?retryWrites=true&w=majority"
